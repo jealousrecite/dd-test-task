92e67b6538706d19a10c32bc6fe6b16043ac9e14
jealousrecite <mailbox19900@gmail.com>
2021-03-19 10:32:44 +0300

    add more validity tests

--- src/test/java/ValidityTester.java
+++ src/test/java/ValidityTester.java
@@ -19,6 +19,11 @@ public class ValidityTester {
     }
 
     @Test(expected = IllegalArgumentException.class)
+    public void missingOpeningBracketStringIsInvalid() {
+        Unpacker.unpack("2abc]");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
     public void missingClosingBracketStringIsInvalid() {
         Unpacker.unpack("2[abc");
     }
@@ -31,5 +36,20 @@ public class ValidityTester {
     @Test(expected = IllegalArgumentException.class)
     public void numbersInInappropriatePlaceStringIsInvalid() {
         Unpacker.unpack("2[ab3c]");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void numbersOnlyStringIsInvalid() {
+        Unpacker.unpack("123");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void nestedForeignSymbolsStringIsInvalid() {
+        Unpacker.unpack("abc2[x&z]def");
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void stringWithSpacesIsInvalid() {
+        Unpacker.unpack("abc 2[xyz]");
     }
 }
