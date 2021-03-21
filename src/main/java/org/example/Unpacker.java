7ba56977019305ea977853bea74e23217b8ef4ce
jealousrecite <mailbox19900@gmail.com>
2021-03-21 10:45:03 +0300

    add user input

--- src/main/java/org/example/Unpacker.java
+++ src/main/java/org/example/Unpacker.java
@@ -4,9 +4,27 @@ package org.example;
 public class Unpacker {
 
     public static void main(String[] args) {
-        var testData = "3[xyz]4[xy]z";
+        if (args.length == 2) {
+            var userInput = args[1];
+            try {
+                System.out.println(Unpacker.unpack(userInput) + "\n");
+            } catch (IllegalArgumentException e) {
+                System.out.println("Invalid input\n"
+                        + e.getMessage() + "\n");
+            }
+        } else if (args.length > 2) {
+            System.out.println("Too many arguments\n");
+            System.out.println(getExample());
+        } else {
+            System.out.println("No arguments provided\n");
+            System.out.println(getExample());
+        }
+    }
 
-        System.out.println("Demo Result: " + unpack(testData));
+    private static String getExample() {
+        var exampleInput = "3[xyz]4[xy]z";
+        return "Example input: " + exampleInput + "\n"
+                + "Example output: " + Unpacker.unpack(exampleInput) + "\n";
     }
 
     public static String unpack(String s) throws IllegalArgumentException {
